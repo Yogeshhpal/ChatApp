3e0085350099fc186ffa54d33ce843c98d4522a4
Yogesh Pal <yogesh.ygs999@gmail.com>
2024-07-09 17:51:01 +0530

    _________

--- backend/helpers/getUserDetailsFromToken.js
+++ backend/helpers/getUserDetailsFromToken.js
@@ -10,7 +10,7 @@ const getUserDetailsFromToken = async (token) => {
         }
     }
 
-    const decode = await jwt.verify(token, process.env.JWT_SECRET);
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await UserModel.findById(decode.id).select('-password')
 
